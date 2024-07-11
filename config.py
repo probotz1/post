2ada72c07618bbfd9c0dd3c619120d2d385abb1a
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-07-11 01:42:33 -1200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_ID = 'your_api_id'
-API_HASH = 'your_api_hash'
-BOT_TOKEN = 'your_bot_token'
-CHANNEL_ID = 'your_channel_id'  # ID of the channel where the posts will be made
+API_ID = '28015531'
+API_HASH = '2ab4ba37fd5d9ebf1353328fc915ad28'
+BOT_TOKEN = '6637112532:AAG_DQYJXytpBLFZAKbNF44AGQulzg6julM'
+CHANNEL_ID = '-1002154419516'  # ID of the channel where the posts will be made
